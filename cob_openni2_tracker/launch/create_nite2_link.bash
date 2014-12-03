f2367aa17d8861d96b9e729d61951c45285c7f56
rmb-om <rmb-om@rbormann-WS558-Ubuntu.(none)>
2014-12-03 19:59:37 +0100

    added a link creation script to allow NiTE to find its data files

--- cob_openni2_tracker/launch/create_nite2_link.bash
+++ cob_openni2_tracker/launch/create_nite2_link.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-NITE_DIR=$(rospack find cob_openni2_tracker)
-#echo $NITE_DIR
-ln -s $NITE_DIR/include/NiTE-Linux-x64-2.2/Redist/NiTE2/  ~/.ros/NiTE2
+NITE_DIR=$(rospack find libnite2)/common/lib/NiTE2/
+echo NiTE_DIR = $NITE_DIR
+
+LINK_NAME=$HOME/.ros/NiTE2
+echo LINK_NAME = $LINK_NAME
+
+if [ -d $LINK_NAME ]; then
+	echo  "Link $LINK_NAME to $NITE_DIR already exists."
+else
+	ln -s $NITE_DIR $LINK_NAME
+	echo "Created link $LINK_NAME to $NITE_DIR."
+fi
