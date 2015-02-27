94ccbdf40ff834af8e3f377b6ac1632f211fd416
Alexander Gutenkunst <alex.gutenkunst+ipa-frm-ag@gmail.com>
2015-02-27 10:09:15 +0100

    Extra header for bash color definitions.

---
+++ include/leg_detector/color_definitions.h
@@ -0,0 +1,52 @@
+/*
+ * color_definitions.h
+ *
+ *  Created on: Feb 27, 2015
+ *      Author: frm-ag
+ */
+
+#ifndef PEOPLE_LEG_DETECTOR_INCLUDE_LEG_DETECTOR_COLOR_DEFINITIONS_H_
+#define PEOPLE_LEG_DETECTOR_INCLUDE_LEG_DETECTOR_COLOR_DEFINITIONS_H_
+
+    #ifdef USE_BASH_COLORS
+
+        #define RESET   "\033[0m"
+        #define BLACK   "\033[30m"      /* Black */
+        #define RED     "\033[31m"      /* Red */
+        #define GREEN   "\033[32m"      /* Green */
+        #define YELLOW  "\033[33m"      /* Yellow */
+        #define BLUE    "\033[34m"      /* Blue */
+        #define MAGENTA "\033[35m"      /* Magenta */
+        #define CYAN    "\033[36m"      /* Cyan */
+        #define WHITE   "\033[37m"      /* White */
+        #define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+        #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+        #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+        #define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+        #define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+        #define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+        #define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+        #define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+    #else
+
+        #define RESET   ""
+        #define BLACK   ""      /* Black */
+        #define RED     ""      /* Red */
+        #define GREEN   ""      /* Green */
+        #define YELLOW  ""      /* Yellow */
+        #define BLUE    ""      /* Blue */
+        #define MAGENTA ""      /* Magenta */
+        #define CYAN    ""      /* Cyan */
+        #define WHITE   ""      /* White */
+        #define BOLDBLACK   ""      /* Bold Black */
+        #define BOLDRED     ""      /* Bold Red */
+        #define BOLDGREEN   ""      /* Bold Green */
+        #define BOLDYELLOW  ""      /* Bold Yellow */
+        #define BOLDBLUE    ""      /* Bold Blue */
+        #define BOLDMAGENTA ""      /* Bold Magenta */
+        #define BOLDCYAN    ""      /* Bold Cyan */
+        #define BOLDWHITE   ""      /* Bold White */
+
+    #endif
+#endif /* PEOPLE_LEG_DETECTOR_INCLUDE_LEG_DETECTOR_COLOR_DEFINITIONS_H_ */
--- src/training_set_creator.cpp
+++ src/training_set_creator.cpp
@@ -71,23 +71,8 @@
 
 #define foreach BOOST_FOREACH
 
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+#define USE_BASH_COLORS
+#include <leg_detector/color_definitions.h>
 
 //#include <leg_detector/debug.h>
 
@@ -263,6 +248,7 @@ public:
                     ros::spinOnce();
                     ros::spinOnce();
 
+                    // Insert this clusters with their labels into the map
                     clusterTimeMap_.insert(std::pair<ros::Time, list<SampleSet*>*>(m.getTime(),pClusters));
                     cout << "Inserted into map!" << endl;
 
