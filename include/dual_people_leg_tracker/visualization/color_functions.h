9263361288c6e1f05d05f31b0ab08b08d8ea146d
Alexander Gutenkunst <alex.gutenkunst+ipa-frm-ag@gmail.com>
2015-04-27 18:06:19 +0200

    New visualizations

---
+++ include/dual_people_leg_tracker/visualization/color_functions.h
@@ -0,0 +1,30 @@
+/*
+ * color_functions.h
+ *
+ *  Created on: Apr 27, 2015
+ *      Author: frm-ag
+ */
+
+#ifndef PEOPLE_DUAL_PEOPLE_LEG_TRACKER_INCLUDE_DUAL_PEOPLE_LEG_TRACKER_VISUALIZATION_COLOR_FUNCTIONS_H_
+#define PEOPLE_DUAL_PEOPLE_LEG_TRACKER_INCLUDE_DUAL_PEOPLE_LEG_TRACKER_VISUALIZATION_COLOR_FUNCTIONS_H_
+
+#include <math.h>
+
+void getColor(int value, int &red, int &green, int &blue)
+{
+  int N = 20;
+
+  value = value % N;
+
+  double h;
+  double r = 0.9999;
+  h = 1.0/N;
+
+  red   = (r * cos(2*M_PI*value/N)*0.5 + 0.5)*255;
+  green = (r * sin(2*M_PI*value/N)*0.5 + 0.5)*255;
+  blue  = h*value * 255;
+
+  std::cout << value << " :" << red << " " << green << " " << blue << std::endl;
+}
+
+#endif /* PEOPLE_DUAL_PEOPLE_LEG_TRACKER_INCLUDE_DUAL_PEOPLE_LEG_TRACKER_VISUALIZATION_COLOR_FUNCTIONS_H_ */
