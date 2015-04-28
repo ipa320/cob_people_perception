4f5b1314ff442a121dbf41a7d60b2db3a7f68004
Alexander Gutenkunst <alex.gutenkunst+ipa-frm-ag@gmail.com>
2015-04-28 19:32:46 +0200

    Intermediate commit

---
+++ include/dual_people_leg_tracker/math/math_functions.h
@@ -0,0 +1,18 @@
+/*
+ * math_functions.h
+ *
+ *  Created on: Apr 28, 2015
+ *      Author: frm-ag
+ */
+
+#ifndef PEOPLE_DUAL_PEOPLE_LEG_TRACKER_INCLUDE_DUAL_PEOPLE_LEG_TRACKER_MATH_MATH_FUNCTIONS_H_
+#define PEOPLE_DUAL_PEOPLE_LEG_TRACKER_INCLUDE_DUAL_PEOPLE_LEG_TRACKER_MATH_MATH_FUNCTIONS_H_
+
+#include <math.h>
+
+double sigmoid(double x, double a=1.0, double b=0.0)
+{
+  return 1 / (1 + exp(-a*(x-b)));
+}
+
+#endif /* PEOPLE_DUAL_PEOPLE_LEG_TRACKER_INCLUDE_DUAL_PEOPLE_LEG_TRACKER_MATH_MATH_FUNCTIONS_H_ */
