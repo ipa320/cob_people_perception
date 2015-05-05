0e28646f399139686eee88666a2e0e70612498a0
Alexander Gutenkunst <alex.gutenkunst+ipa-frm-ag@gmail.com>
2015-05-05 10:42:11 +0200

    Worked on better visualizations. High level model association done.

--- include/dual_people_leg_tracker/math/math_functions.h
+++ include/dual_people_leg_tracker/math/math_functions.h
@@ -15,4 +15,34 @@ double sigmoid(double x, double a=1.0, double b=0.0)
   return 1.0 / (1.0 + exp(-a*(x-b)));
 }
 
+double distance(tf::Vector3 point, tf::Vector3 pointOnLine, tf::Vector3 lineVector){
+
+  // Normalize
+  // lineVector = lineVector.normalize();
+
+  // Orthogonal vector
+  tf::Vector3 v;
+  v[0] = lineVector[1];
+  v[1] = -lineVector[0];
+  v[2] = 0.0;
+
+  v = v.normalize();
+
+  //std::cout << "LineVector: " << lineVector[0] << "  " << lineVector[1] << "  " << lineVector[2] << std::endl;
+  //std::cout << "V         : " << v[0] << "  " << v[1] << "  " << v[2] << std::endl;
+
+  // Check that v is really orthogonal
+  ROS_ASSERT(lineVector.dot(v) < 0.00001);
+
+  //std::cout << "V: " << lineVector.dot(v) << std::endl;
+
+  double distance = (lineVector.cross(pointOnLine-point)).length()/lineVector.length();
+
+  // tf::Vector3 crossPoint;
+  // crossPoint = point + distance * v;
+  // assert(false);
+
+  return distance;
+}
+
 #endif /* PEOPLE_DUAL_PEOPLE_LEG_TRACKER_INCLUDE_DUAL_PEOPLE_LEG_TRACKER_MATH_MATH_FUNCTIONS_H_ */
