cf5886fe11525a91f53118b3a7aaa21d46063ae6
Alexander Gutenkunst <alex.gutenkunst+ipa-frm-ag@gmail.com>
2015-05-07 18:35:27 +0200

    Intermediate commit. Fixed wrong people model.

--- include/dual_people_leg_tracker/math/math_functions.h
+++ include/dual_people_leg_tracker/math/math_functions.h
@@ -15,32 +15,21 @@ double sigmoid(double x, double a=1.0, double b=0.0)
   return 1.0 / (1.0 + exp(-a*(x-b)));
 }
 
+/**
+ * Distance between a point and the line define by a point and a vector
+ * @param point The point the distance is measured towards
+ * @param pointOnLine An arbitrary point on the line
+ * @param lineVector Vector of the line
+ * @return
+ */
 double distance(tf::Vector3 point, tf::Vector3 pointOnLine, tf::Vector3 lineVector){
 
   // Normalize
-  // lineVector = lineVector.normalize();
+  lineVector = lineVector.normalize();
 
-  // Orthogonal vector
-  tf::Vector3 v;
-  v[0] = lineVector[1];
-  v[1] = -lineVector[0];
-  v[2] = 0.0;
+  tf::Vector3 ap = pointOnLine - point;
 
-  v = v.normalize();
-
-  //std::cout << "LineVector: " << lineVector[0] << "  " << lineVector[1] << "  " << lineVector[2] << std::endl;
-  //std::cout << "V         : " << v[0] << "  " << v[1] << "  " << v[2] << std::endl;
-
-  // Check that v is really orthogonal
-  ROS_ASSERT(lineVector.dot(v) < 0.00001);
-
-  //std::cout << "V: " << lineVector.dot(v) << std::endl;
-
-  double distance = (lineVector.cross(pointOnLine-point)).length()/lineVector.length();
-
-  // tf::Vector3 crossPoint;
-  // crossPoint = point + distance * v;
-  // assert(false);
+  double distance = (ap-(ap.dot(lineVector))*lineVector).length();
 
   return distance;
 }
