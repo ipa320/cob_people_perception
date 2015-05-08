e49c05d8b329a6b0944ad6eb5a9556f4f513e479
Alexander Gutenkunst <alex.gutenkunst+ipa-frm-ag@gmail.com>
2015-05-08 13:06:36 +0200

    Added a occlusion model. Needed to reduce the number of particles in order to use the model.

---
+++ src/occlusion_model.cpp
@@ -0,0 +1,55 @@
+#include <people_tracking_filter/occlusion_model.h>
+#include <cmath>
+
+OcclusionModel::OcclusionModel( tf::TransformListener& tfl)
+:tfl_(tfl)
+{
+
+}
+
+void OcclusionModel::updateScan(const sensor_msgs::LaserScan& scan){
+  ROS_DEBUG_COND(DEBUG_OCCLUSION_MODEL,"OcclusionModel::%s",__func__);
+  scan_ = scan;
+}
+
+double OcclusionModel::getOcclusionProbability(tf::Stamped<tf::Point> point){
+  ROS_DEBUG_COND(DEBUG_OCCLUSION_MODEL,"OcclusionModel::%s",__func__);
+
+  // Transform the point into the coordinate system of the laserscan
+  try
+  {
+    tfl_.transformPoint(scan_.header.frame_id, point, point); //Transform using odometry information into the fixed frame
+    ROS_DEBUG_COND(DEBUG_OCCLUSION_MODEL,"OcclusionModel::%s - Transforming point from %s to %s",__func__, point.frame_id_.c_str(), scan_.header.frame_id.c_str());
+  }
+  catch (...)
+  {
+    ROS_WARN("TF exception spot 3.");
+  }
+
+  double angle  = atan2(point[1],point[0]);
+  double r      = point.length();
+
+  if(angle > scan_.angle_min && angle < scan_.angle_max){
+    double distance_to_angle_min = angle - scan_.angle_min;
+    int range_idx = distance_to_angle_min/scan_.angle_increment;
+
+    // The ranges left and right of the scanpoint
+    double r0 = scan_.ranges[range_idx];
+    double r1 = scan_.ranges[range_idx+1];
+
+
+    if(r0 < r && r1 < r){
+      return 1.0; // Total shadow
+    }
+
+    if(r0 > r && r1 > r){
+      return 0.0; // should be observable
+    }
+
+    //In other cases return 0.5
+    return 0.5;
+
+  }
+
+}
+
