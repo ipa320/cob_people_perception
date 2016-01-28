c351a89944ce0bfe6133178279072d17e2966d7a
Alexander Gutenkunst <alex.gutenkunst+ipa-frm-ag@gmail.com>
2016-01-28 13:13:43 +0100

    Ensure that the color gradient gets the right input range. Otherwise warn user.

--- include/people_fusion_node/visualization/color_gradient.hpp
+++ include/people_fusion_node/visualization/color_gradient.hpp
@@ -1,9 +1,20 @@
 #ifndef COLOR_GRADIENT_HPP_
 #define COLOR_GRADIENT_HPP_
 
+/**
+ * Maps a given value [0..1] to r,g,b within range [0..255]
+ * @param value The given value
+ * @param red
+ * @param green
+ * @param blue
+ */
 void redGreenGradient(double value, int &red, int &green, int &blue)
 {
-  //std::cout << "Value: " << value << std::endl;
+
+  ROS_WARN_COND(value < 0 || value > 1, "redGreenGradient expects value in range [0..1]!");
+
+  // Ensure right range
+  double v = std::max(std::min(value,1.0),0.0);
 
   int aR = 255;   int aG = 0; int aB=0;  // RGB for our 1st color (blue in this case).
   int bR = 0; int bG = 255; int bB=0;    // RGB for our 2nd color (red in this case).
